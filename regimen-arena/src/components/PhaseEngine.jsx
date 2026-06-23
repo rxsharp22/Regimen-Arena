6bc8f0f21b101f8cca520e1bb6d1614e12aece35
Russell <russellsharp8@gmail.com>
2026-06-22 21:32:40 -0500

    Update PhaseEngine.jsx

--- regimen-arena/src/components/PhaseEngine.jsx
+++ regimen-arena/src/components/PhaseEngine.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState, useEffect, useCallback } from 'react'
+import { useMemo, useState, useEffect, useCallback, useRef } from 'react'
 import PhaseHeader from './PhaseHeader'
 import PatientStatusPanel from './PatientStatusPanel'
 import NewInformationPanel from './NewInformationPanel'
